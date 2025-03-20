134df7ac29ff7208db7ab0dbe3716d58d31e6fbf
Yaroslav <61938366+yarov3so@users.noreply.github.com>
2025-03-20 14:56:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@
 import re
 import pandas as pd
 import math
+import streamlit as st
 
-print("\n")
+st.title("Percentile Rank Calculator")
 
 def comprehend(mystring):
     mystring=mystring.replace(" ", "")
@@ -19,9 +20,16 @@ def comprehend(mystring):
                 data.append(None)
     return data
 
-print("Percentile Rank Calculator by yarov3so\n")
+st.title("Percentile Rank Calculator by yarov3so\n")
+st.markdown("*by yarov3so*")
+st.text("Calculates the percentile rank of all values in a data set. Supports suppressed value ranges. Finds values corresponding to a given percentile rank.")
 
-data=comprehend(input("Enter all the data values in increasing order, separated by commas. For suppressed entries, enter how many there are in each group between two underscores: \"_{ number of suppressed entries in the group}_\". Note that the values within each group of suppressed entries are assumed to be strictly between the two values that the group is sandwiched between. \n\n") )
+data=st.text_input("Enter all the data values in increasing order, separated by commas. For suppressed entries, enter how many there are in each group between two underscores: \"_{ number of suppressed entries in the group}_\". Note that the values within each group of suppressed entries are assumed to be strictly between the two values that the group is sandwiched between.")
+
+if data=="":
+    st.stop()
+    
+data=comprehend(data)
 
 while True:
     data_check=[]
@@ -29,16 +37,16 @@ while True:
         if el!=None:
             data_check.append(el)
     if sorted(data_check)!=data_check:
-        print("\n")
-        data=comprehend(input("You did not enter your values in increasing order! Check the order and try again. \n\n"))
+        data=st.text_input(("You did not enter your values in increasing order! Check the order and try again."))
+        if data=="":
+            st.stop()
+        data=comprehend(data)
     else:
-        print("\n")
         break
-    
-print(f"Your data set has a total of {len(data)} entries, of which {len(data)-data.count(None)} are made explicit. \n")
-print(f"The explicitly stated entries have values:",", ".join([str(num) for num in sorted([int(el) if int(el)==el else el for el in set(data)-{None}])]))
-
-print("\n")
+        
+st.text(f"Your data set has a total of {len(data)} entries, of which {len(data)-data.count(None)} are made explicit.")
+output="The explicitly stated entries have values: "+", ".join([str(num) for num in sorted([int(el) if int(el)==el else el for el in set(data)-{None}])])
+st.text(f"{output}")
 
 df=pd.DataFrame(columns=["Value","Percentile Rank"])
 
@@ -51,5 +59,4 @@ df=df.sort_values(by='Value', ascending=True).reset_index(drop=True)
 if set([float(int(el)) for el in set(data)-{None}])==set(data)-{None}:
     df["Value"]=df["Value"].astype(int)
 
-print(df.to_string(index=False))
-print("\n")
+st.dataframe(df)
