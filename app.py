8298bfa18c306b6773c6458fa8b6fb1c1def341f
Yaro <61938366+yarov3so@users.noreply.github.com>
2025-03-27 20:32:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ val_flt=val[0]
 val=try_int(val[0])
 
 if val not in list(df["Value"].astype(float)):
-    st.warning("The value you have entered does not explicitly appear in the data set!")
+    st.warning("The value you have entered does not explicitly appear in the data set! As such, its percentile rank cannot be calculated.")
     st.stop()
 
 st.markdown(f"$ \\text{{PR}} ({val}) = \\left[ \ \\left( \\frac{{ ( \ \# \ \\text{{entries}} \ < \ {val} \ ) \ + \ \\frac{{1}}{{2}}( \ \# \ \\text{{entries}} \ = \ {val} \ ) }}{{ \# \ \\text{{entries in total}} }} \\right) \cdot 100 \ \\right] = \\left[ \ \\left( \\frac{{ {data.index(val_flt)} \ + \ \\frac{{1}}{{2}}( \ {data.count(val_flt)} \ ) }}{{ {len(data)} }} \\right) \cdot 100 \ \\right] = \\left[ {try_int(100*(data.index(val_flt) + 0.5*data.count(val_flt))/len(data))} \\right] = {math.ceil(try_int(100*(data.index(val_flt) + 0.5*data.count(val_flt))/len(data)))}  $")
